4e30782c502a14e63f9914e1ee1f09d37c89a15d
Boussouriou <54223302+diallo99bouss@users.noreply.github.com>
2024-03-28 00:09:09 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,70 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true //
+  
 })
 export class AppComponent {
-  title = 'yadein';
+  isActive: boolean = true;
+  title: string = 'yadein';
+
+//Sidebar toggle show hide function
+status = false;
+addToggle()
+{
+  this.status = !this.status;       
+}
+
+//Charts
+type = 'line';
+type2 = 'bar';
+dataa = {
+  labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+  datasets: [{
+          label: "Salse",
+          data: [15, 30, 55, 45, 70, 65, 85],
+          backgroundColor: "rgba(235, 22, 22, .7)",
+          fill: true
+      },
+      {
+          label: "Revenue",
+          data: [99, 135, 170, 130, 190, 180, 270],
+          backgroundColor: "rgba(235, 22, 22, .5)",
+          fill: true
+      }
+  ]
+  }
+  dataaa = {
+    labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+    datasets: [{
+            label: "Salse",
+            data: [15, 30, 55, 45, 70, 65, 85],
+            backgroundColor: "rgba(235, 22, 22, .7)",
+            fill: true
+        },
+        {
+            label: "Revenue",
+            data: [99, 135, 170, 130, 190, 180, 270],
+            backgroundColor: "rgba(235, 22, 22, .5)",
+            fill: true
+        }
+    ]
+    }
+         options = {
+           
+            
+            maintainAspectRatio: true,
+            scales: {
+                yAxes : [{
+                    ticks : {
+                        max : 90,    
+                        min : 30
+                    }
+                }]
+            }
+        };
+
 }
